72ff5d500a7cb2d47808021d68889200d73ed6d9
lbtska <labetska.lilichka@gmail.com>
2024-12-08 12:46:13 +0200

    Delete lab_08 directory

--- lab_08/lab_08/script.js
+++
@@ -1,8 +0,0 @@
-function toggleSubFiles(id) {
-    const element = document.getElementById(id);
-    if (element.classList.contains('hidden')) {
-      element.classList.remove('hidden');
-    } else {
-      element.classList.add('hidden');
-    }
-  }
